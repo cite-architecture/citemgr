864a8b37bd585afeb71d49c4f461c319523d0f20
Neel Smith <dnsmith.neel@gmail.com>
2016-06-07 15:05:35 -0400

    Test text-surface relation

--- core/src/test/groovy/org/homermultitext/citemanager/TestDseManager.groovy
+++ core/src/test/groovy/org/homermultitext/citemanager/TestDseManager.groovy
@@ -14,8 +14,9 @@ class TestDseManager {
 
   def textToImageFiles = [new File("testdata/venA17-textToImage-Iliad.csv")]
   def testImage = "urn:cite:hmt:vaimg.VA223VN-0725"
-  def textToSurfaceFiles = [new File("testdata/venA17-textToFolio-Iliad.csv")]
 
+  def textToSurfaceFiles = [new File("testdata/venA17-textToFolio-Iliad.csv")]
+  def testFolio = "urn:cite:hmt:msA.224v"
   @Test
   void testTextImageRelation() {
     DseManager dsem = new DseManager()
@@ -46,13 +47,25 @@ class TestDseManager {
 
   @Test
   void testTextSurfaceRelation() {
-    //
+    DseManager dsem = new DseManager()
+    dsem.textTbsIndexFiles = textToSurfaceFiles
+
+
+    def expectedUrns = [new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.63"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.64"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.65"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.66"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.67"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.68"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.69"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.70"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.71"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.72"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.73"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.74"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.75"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.61"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.62"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.51"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.52"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.53"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.54"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.55"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.56"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.57"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.58"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.59"), new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:17.60")]
+
+    def actualUrns = dsem.textNodesForSurface(testFolio)
+    assert expectedUrns.size() == actualUrns.size()
+
+    actualUrns = dsem.textNodesForSurface(new CiteUrn(testFolio))
+    assert expectedUrns.size() == actualUrns.size()
+
+
   }
 
 /*
 
 
-scholiaInventories = "/vagrant/hc-il17/collections/venA-exterior.csv,/vagrant/hc-il17/collections/venA-intermarginal.csv,/vagrant/hc-il17/collections/venA-interior.csv,/vagrant/hc-il17/collections/venA-mainScholia.csv,/vagrant/hc-il17/collections/venA-interlinear.csv"
+scholiaInventories = "/vagrant/hc-il17/collections/venA-exterior.csv"),/vagrant/hc-il17/collections/venA-intermarginal.csv,/vagrant/hc-il17/collections/venA-interior.csv,/vagrant/hc-il17/collections/venA-mainScholia.csv,/vagrant/hc-il17/collections/venA-interlinear.csv"
 */
 
 }
